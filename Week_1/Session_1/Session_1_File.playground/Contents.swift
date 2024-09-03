744f586ece98a0acffa7c71eff0fe52643908a8a
hartejsingh12 <12.singh.hartej@gmail.com>
2024-09-02 22:19:42 -0400

    Initialized repository

---
+++ Week_1/Session_1/Session_1_File.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Cocoa
+
+//-------------------------------------------------------------------------//Part 1 - Instructions\\-------------------------------------------------------------------\\
+
+"""
+Hello and welcome to the first mini-challenge of the bootcamp!
+If not already done so, please check the Github README for instructions on today's mini-challenge!
+Additionally, please feel free to ask your instructor if you need any help!
+"""
+
+//---------------------------------------------------------------------------//Part 1 - Coding\\-----------------------------------------------------------------------\\
+
+//1)
+
+//2)
+
+//3)
+
+//--------------------------------------------------------------------------//Part 1 - Abstract\\----------------------------------------------------------------------\\
+
+
+
