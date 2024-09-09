27d6e09eca4ee210454f4a15982e85e679d8bd9b
“anjali-m29” <anjali@lawn-10-91-34-173.lawn.gatech.edu>
2024-09-09 19:31:24 -0400

    Completed Task 1

--- Week_One/Session_One/TODO.playground/Contents.swift
+++ Week_One/Session_One/TODO.playground/Contents.swift
@@ -9,8 +9,32 @@ Additionally, please feel free to ask your instructor if you need any help!
 //----------------------------------------------------------------------------//Part 2 - Coding\\----------------------------------------------------------------------\\
 
 //1)
-
+var greeting: String = "Hello, World!"
+let pi = 3.14159
 //2)
-
+var num = 11
+if (num > 10) {
+    print("Number is greater than 10")
+} else {
+    print("Number is 10 or less")
+}
 //3)
+var a = 5
+var b = 3
 
+var sum = a + b
+print(sum)
+var difference = a - b
+print(difference)
+var product = a * b
+print(product)
+var quotient = a / b
+print(quotient)
+
+guard a >= 5 else {
+    fatalError("Exiting program")
+}
+
+guard a > 4 || b < 3 else {
+    fatalError("Exiting program")
+}
