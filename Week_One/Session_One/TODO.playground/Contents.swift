4b5d211c1072b39f3603c1c4428b1945ffb3fd20
thomasg13 <thomasguo13@gmail.com>
2024-09-09 19:57:44 -0400

    finished

--- Week_One/Session_One/TODO.playground/Contents.swift
+++ Week_One/Session_One/TODO.playground/Contents.swift
@@ -9,8 +9,35 @@ Additionally, please feel free to ask your instructor if you need any help!
 //----------------------------------------------------------------------------//Part 2 - Coding\\----------------------------------------------------------------------\\
 
 //1)
+var greeting = "Hello, World!"
+let pi = 3.14159
 
 //2)
+let num = 12
+if(num > 10){
+    print("Number is greater than 10")
+}else{
+    print("Number is 10 or less")
+}
 
 //3)
+var a = 5
+var b = 3
+var sum = a + b
+print(sum)
+var difference = a - b
+print(difference)
+var product = a * b
+print(product)
+var quotient = a / b
+print(quotient)
+
+guard a >= 5 else{
+    fatalError("Exiting program")
+}
+
+guard a > 4 || b < 3 else{
+    fatalError("Exiting program")
+}
+
 
