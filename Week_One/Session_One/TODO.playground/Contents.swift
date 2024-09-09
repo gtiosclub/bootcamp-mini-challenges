2ed61cdabe2f120f860c0cc515fafed5a6e09fc2
Magnolia Wei <magnoliawei999@gmail.com>
2024-09-09 19:52:41 -0400

    Finished tasks

--- Week_One/Session_One/TODO.playground/Contents.swift
+++ Week_One/Session_One/TODO.playground/Contents.swift
@@ -9,8 +9,26 @@ Additionally, please feel free to ask your instructor if you need any help!
 //----------------------------------------------------------------------------//Part 2 - Coding\\----------------------------------------------------------------------\\
 
 //1)
-
+var greeting = "Hello, World!"
+let pi = 3.14159
 //2)
-
+var num = 2
+if num>10{
+    print("Number is greater than 10")
+}
+else{
+    print("Number is 10 or less")
+}
 //3)
-
+var a = 5
+var b = 3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+guard a>=5 else{
+    fatalError("Exiting program")
+}
+guard a>4 || b<3 else{
+    fatalError("Exiting program")
+}
