9ea098cce5a0c85ab28ecaf4c63bead119792f2c
Lydia He <lyhe1106@gmail.com>
2024-09-10 19:46:45 -0400

    idk

--- Week_One/Session_One/TODO.playground/Contents.swift
+++ Week_One/Session_One/TODO.playground/Contents.swift
@@ -9,8 +9,20 @@ Additionally, please feel free to ask your instructor if you need any help!
 //----------------------------------------------------------------------------//Part 2 - Coding\\----------------------------------------------------------------------\\
 
 //1)
+var greeting: String = "Hello, World!"
+let pi: Float = 3.14159
 
 //2)
+var Num : Int=1
+if Num > 10{
+    print("Number is greater than 10")
+}
+else {
+    print("Number is 10 or less")
+}
 
 //3)
+var a : Int = 5
+var b : Int = 3
+
 
