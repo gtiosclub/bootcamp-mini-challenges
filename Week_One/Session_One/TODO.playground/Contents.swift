35b905f3f052b0c2a6accb7b7159e00196712c3a
Lexiiy <zijie.yan1019@gmail.com>
2024-09-10 19:47:06 -0400

    Done Mini Challenge

--- Week_One/Session_One/TODO.playground/Contents.swift
+++ Week_One/Session_One/TODO.playground/Contents.swift
@@ -9,8 +9,29 @@ Additionally, please feel free to ask your instructor if you need any help!
 //----------------------------------------------------------------------------//Part 2 - Coding\\----------------------------------------------------------------------\\
 
 //1)
-
+var greeting = "Hello, World!"
+let pi = 3.14159
 //2)
-
+var number = 5
+if number > 10 {
+    print("Number is greter than 10")
+} else {
+    print("Number is 10 or less")
+}
 //3)
-
+var a = 5
+var b = 3
+var sum = a + b
+print(sum)
+var difference = a - b
+print(difference)
+var product = a * b
+print(product)
+var quotient = a / b
+print(quotient)
+guard a >= 5 else {
+    fatalError("Exiting program")
+}
+guard a > 4 || b < 3 else {
+    fatalError("Exiting program")
+}
