568c12a7cf12e3f9dac2583646fc7c2bf78d9b45
lizyxv <chihyinglilee@gmail.com>
2024-09-10 22:48:54 -0400

    W1S1 challeneg

--- Week_One/Session_One/TODO.playground/Contents.swift
+++ Week_One/Session_One/TODO.playground/Contents.swift
@@ -11,5 +11,16 @@ Additionally, please feel free to ask your instructor if you need any help!
 var greeting: String = "Hello, World!"
 let pi: Double = 3.14159
 
+if (pi > 10) {print("Number is greater than 10")}
+else {"Number is 10 or less"}
 
+var a: Int = 5
+var b: Int = 3
 
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+guard (a>=5) else{fatalError("Exiting program")}
+guard (a>4||b<3) else{fatalError("Exiting program")}
