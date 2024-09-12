4f765cb85ec4e05f63003ca64953b54419416551
Matthew Dong <mattdong1231@gmail.com>
2024-09-12 19:57:40 -0400

    mini-challenges 2

--- Week_One/Session_Two/TODO.playground/Contents.swift
+++ Week_One/Session_Two/TODO.playground/Contents.swift
@@ -9,8 +9,37 @@ Additionally, please feel free to ask your instructor if you need any help!
 //----------------------------------------------------------------------------//Part 2 - Coding\\----------------------------------------------------------------------\\
 
 //1)
-
+var dogName: String? = nil
+var catName: String = "Dora"
 //2)
+var myHouse: String? = nil
+var myTV: String? = "tv"
+let myTVUnwrapped: String = myTV!
+let myHouseUnwrapped: String = myHouse!
+
+
 
 //3)
+var price: Int? = nil
+var myFavFruit: String? = "Strawberry"
+if let latestPrice = price { // If price (right hand side) is not nil, unwrap it (make it non-Optional), and set it to a new variable `latestPrice` to be used inside the brackets ONLY
+    print(latestPrice)
+}
+
+// ====
+
+if price != nil {
+    let latestPrice = price!
+    
+    print(latestPrice)
+}
+
+//4)
+let defaultVal = 0
+var realPrice: Int = price ?? defaultVal
+guard let myPrice = price, let myFruit = myFavFruit else {
+    print("Unavailable")
+    fatalError()
+}
+print("The \(myFruit)'s price is \(myPrice)")
 
