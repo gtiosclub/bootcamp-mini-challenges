49cc9ed8a16b31d5cc85ca012892a595961657cb
rbokil <rini.bokil@gmail.com>
2024-09-12 19:20:14 -0400

    completed mini challenge 3

--- Week_One/Session_Two/TODO.playground/Contents.swift
+++ Week_One/Session_Two/TODO.playground/Contents.swift
@@ -16,9 +16,33 @@ var catName : String? = "Dora"
 var myHouse : String? = nil
 var myTV : String? = ""
 let myTVUnwrapped = myTV!
-let myHouseUnwrapped = myHouse!
+//let myHouseUnwrapped = myHouse!
 
 // got a "the playground encountered a crash and could not finish executing error. happened because we unwrapped myHouse, which contained a nil value. however, unwrapping myTV was fine because while it was an optional, it didn't contain a nil value.
 
 //3)
+var price : Int? = nil
+var myFavFruit : String? = "Strawberry"
 
+if let tempPrice = price {
+    print("This item's price is \(price)")
+} else {
+    print("This item is not for sale!.")
+}
+if let tempFruit = myFavFruit, let tempPrice = price {
+    print ("The \(myFavFruit)'s price is \(price)")
+} else {
+    print ("Error!")
+}
+//4)
+guard let priceUnwrapped = price else {
+    print ("Unavailable.")
+    fatalError()
+}
+print ("This item's price is \(priceUnwrapped)")
+guard let fruitUnwrapped = myFavFruit, let priceUnwrapped = price else {
+    print ("Unavailable.")
+    fatalError()
+}
+print ("The \(myFavFruit)'s price is \(priceUnwrapped)")
+var realPrice = price ?? 0
