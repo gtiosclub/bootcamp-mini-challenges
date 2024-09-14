618cc8de90672572e5464e518cdae1832912cffc
gellenee <glennedelatorre@gmail.com>
2024-09-14 19:38:08 -0400

    module

--- Week_One/Session_Two/TODO.playground/Contents.swift
+++ Week_One/Session_Two/TODO.playground/Contents.swift
@@ -9,8 +9,52 @@ Additionally, please feel free to ask your instructor if you need any help!
 //----------------------------------------------------------------------------//Part 2 - Coding\\----------------------------------------------------------------------\\
 
 //1)
+var dogName: String? = nil
+var catName: String? = "Dora"
 
 //2)
-
+var myHouse: String? = nil
+var myTV: String?
+let myTVUnwrapped = myTV! // force unwrapping
+let myHouseUnwrapped = myHouse!
 //3)
 
+// optional binding - takes peek inside then unwraps if it exists
+// if let statement
+var price: Int? = nil
+var myFavFruit: String? = "Strawberry"
+if let price{ // go through this
+    print("This item's price is \(price)")
+}else{
+    print("This item is not for sale!")
+}
+if let tempMyFavFruit = myFavFruit, let tempPrice = price{
+    print("The \(tempMyFavFruit)'s price is \(tempPrice) ")
+}else{
+    print("Error")
+}
+
+//4)
+var fallBack = 0
+var realPrice = price ?? fallBack
+guard let unwrappedPrice = price else{ // only works when condition == nil, can be used after
+    print("Unavailable")
+    fatalError()
+}
+guard let unwrappedFruit = myFavFruit else{ // only works when condition == nil, can be used after
+    print("Unavailable")
+    fatalError()
+}
+
+print("The \(unwrappedFruit)'s price is \(unwrappedPrice)")
+
+    
+// fallback using ??
+
+var hashMap : [Int, String] = [:] // initialize empty dict
+
+// diff function syntadx for taking parameteres and returns
+// use -> for when there is a return and put returnType
+// parameter - used in the input inside function
+// argument - used when called outside, cant be used inside function
+        
