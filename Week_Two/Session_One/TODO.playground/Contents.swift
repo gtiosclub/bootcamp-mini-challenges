60ac6cb3192a474381fd9beac37f50634f03df4a
rick-nguyen66 <rick248nguyen@gmail.com>
2024-09-16 18:57:39 -0400

    Tasks 1-3

--- Week_Two/Session_One/TODO.playground/Contents.swift
+++ Week_Two/Session_One/TODO.playground/Contents.swift
@@ -9,8 +9,31 @@ Additionally, please feel free to ask your instructor if you need any help!
 //----------------------------------------------------------------------------//Part 2 - Coding\\----------------------------------------------------------------------\\
 
 //1)
-
+var numbers: [Int] = [1, 2, 3, 4, 5]
+numbers.append(6)
+print(numbers)
+numbers.removeFirst()
+print(numbers)
 //2)
+var dict: [String : Int] = ["John": 90, "Jane": 85, "Jake": 88]
+// dict.updateValue(92, "Jim")
 
 //3)
+var uniqueNumbers: Set<Int> = [1, 2, 3, 4, 5]
+uniqueNumbers.insert(5)
+print (uniqueNumbers)
+
+//4)
+
+//5)
+
+//6)
+
+//7)
+
+
+
+
+
+
 
