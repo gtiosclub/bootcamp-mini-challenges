52566bd126b2e6da2bfb0cc863299f2b27b22b05
“anjali-m29” <anjali@lawn-10-91-85-155.lawn.gatech.edu>
2024-09-16 18:52:33 -0400

    Added

---
+++ Week_Two/Session_One/Contents.swift
@@ -0,0 +1,16 @@
+//-------------------------------------------------------------------------//Part 1 - Instructions\\-------------------------------------------------------------------\\
+
+"""
+Hello and welcome to the first mini-challenge of the bootcamp!
+If not already done so, please check the Github README for instructions on today's mini-challenge!
+Additionally, please feel free to ask your instructor if you need any help!
+"""
+
+//----------------------------------------------------------------------------//Part 2 - Coding\\----------------------------------------------------------------------\\
+
+//1)
+
+//2)
+
+//3)
+
