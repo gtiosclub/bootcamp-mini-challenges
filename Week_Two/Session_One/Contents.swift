50057d62744ecdf9b842c3dc907d46e2619a89dc
“anjali-m29” <anjali@lawn-10-91-85-155.lawn.gatech.edu>
2024-09-16 18:55:38 -0400

    Task 1

--- Week_Two/Session_One/Contents.swift
+++ Week_Two/Session_One/Contents.swift
@@ -9,7 +9,11 @@ Additionally, please feel free to ask your instructor if you need any help!
 //----------------------------------------------------------------------------//Part 2 - Coding\\----------------------------------------------------------------------\\
 
 //1)
-
+var myArray: [Int] = [1, 2, 3, 4, 5]
+myArray.append(6)
+print(myArray)
+myArray.removeFirst()
+print(myArray)
 //2)
 
 //3)
