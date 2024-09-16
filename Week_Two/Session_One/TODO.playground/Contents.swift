5f07172d33db3d9e611f1eb585b815d9bdbb7108
Matthew Dong <mattdong1231@gmail.com>
2024-09-16 19:32:44 -0400

    mini-challenges for session 3

--- Week_Two/Session_One/TODO.playground/Contents.swift
+++ Week_Two/Session_One/TODO.playground/Contents.swift
@@ -10,11 +10,55 @@ Additionally, please feel free to ask your instructor if you need any help!
 
 //1)
 var numbers: [Int] = [1, 2, 3, 4, 5]
-numbers.append(6)
-numbers.remove(at: 0)
+numbers.append(6) //or += 6
+print(numbers)
+numbers.remove(at: 0) // or removeFirst()
 print(numbers)
 
 //2)
 
 //3)
 
+//comprehnsive challenge)
+var myCandies: [String] = []
+myCandies.append(contentsOf: ["Sour Patch", "kit kat", "snickers"])
+var myCandiesSet: Set<String>
+myCandiesSet = Set(myCandies)
+myCandiesSet.formUnion(["reeces", "gummy worms"])
+var months: [Int: String] = [
+    1: "January", 2: "February", 3: "March", 4: "April", 5: "May",
+    6: "June", 7: "July", 8: "August", 9: "September", 10: "October",
+    11: "November", 12: "December"
+]
+
+if let month14 = months[14] {
+    print("Month 14 is \(month14)")
+} else {
+    print("Month 14 is not in the dictionary.")
+}
+
+//4)
+var fruits: [String] = ["Apple", "Banana", "Cherry"]
+for fruit in fruits{
+    print(fruit)
+}
+
+//5)
+var count: Int = 1
+while count <= 5{
+    print(count)
+    count += 1
+}
+
+//6)
+var ages: [String: Int] = ["Alice": 30, "Bob": 25, "Charlie": 35]
+for (person, age) in ages {
+    print("\(person) is \(age)")
+}
+
+
+//7)
+func addNumbers(_ one: Int, _ two: Int) -> Int{
+    return one + two
+}
+print(addNumbers(3, 7))
