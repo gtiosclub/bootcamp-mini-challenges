543a08353c67a3b5fa6e83a3cfb3d7dff6fef265
esanint <esanint05@gmail.com>
2024-09-17 19:02:47 -0400

    Completed Task 1 thru 3

--- Week_Two/Session_One/TODO.playground/Contents.swift
+++ Week_Two/Session_One/TODO.playground/Contents.swift
@@ -9,8 +9,22 @@ Additionally, please feel free to ask your instructor if you need any help!
 //----------------------------------------------------------------------------//Part 2 - Coding\\----------------------------------------------------------------------\\
 
 //1)
+var integers = [1,2,3,4,5]
+integers.append(6)
+print("Array after adding 6:", integers)
+integers.removeFirst()
+print("Array after removing the first element:", integers)
 
 //2)
+var grades = ["John": 90, "Jane": 85, "Jake": 88]
+grades ["Jim"] = 92
+if let janeGrade = grades["Jane"] {
+    print("Jane's grade is \(janeGrade)")
+} else {
+    print("Jane's grade is not available.")
+}
 
 //3)
-
+var uniqueNumbers = Set([1,2,3,4,5])
+uniqueNumbers.insert(5)
+print("Set after adding 5:", uniqueNumbers)
