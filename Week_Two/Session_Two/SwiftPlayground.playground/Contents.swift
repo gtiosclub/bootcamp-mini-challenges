2c6f0e666e40769ad9836f699ac2de8315983220
esanint <esanint05@gmail.com>
2024-09-19 19:21:41 -0400

    Completed task 0.5

--- Week_Two/Session_Two/SwiftPlayground.playground/Contents.swift
+++ Week_Two/Session_Two/SwiftPlayground.playground/Contents.swift
@@ -7,7 +7,26 @@ Additionally, please feel free to ask your instructor if you need any help!
 
 //----------------------------------------------------------------------------//Part 2 - Coding\\----------------------------------------------------------------------\\
 
+//0.5)
+let sayHelloClosureWithParameter = { (name: String) in
+    print("Hello there, \(name)")
+}
+sayHelloClosureWithParameter("Esteban")
+
+let sayHelloClosureWithParameterAndRet = { (name2: String) -> String in
+    return "Hello there, \(name2)"
+}
+sayHelloClosureWithParameterAndRet("Aziz")
+
+func mySeatingArrangement(neighbors: [String]) {
+    print("I am sitting next to \(neighbors[1]) and \(neighbors[2])")
+}
+mySeatingArrangement(neighbors: ["Esteban", "Aziz"])
+
+
+
 //1)
+
 
 //2)
 
