1e9cca5e8d47b8d8812b231d586d819846187dec
rbokil <rini.bokil@gmail.com>
2024-09-19 20:45:41 -0400

    Week 2 mini challenges day 2

--- Week_Two/Session_Two/SwiftPlayground.playground/Contents.swift
+++ Week_Two/Session_Two/SwiftPlayground.playground/Contents.swift
@@ -6,6 +6,43 @@ Additionally, please feel free to ask your instructor if you need any help!
 """
 
 //----------------------------------------------------------------------------//Part 2 - Coding\\----------------------------------------------------------------------\\
+func sayHello() {
+    print("Hello!")
+}
+
+var greet = sayHello
+greet
+greet()
+
+let sayHelloClosure = { print("Hello there!") }
+sayHelloClosure
+sayHelloClosure()
+//slides
+let sayHelloClosureWithParameter = { (str: String) -> () in
+    print("Hello there, \(str)")
+}
+sayHelloClosureWithParameter("Anjali")
+
+let sayHelloClosureWithParameterAndRet = { (str: String) -> (String) in
+    return "Hello there, \(str)"
+}
+
+func mySeatingArrangement (neighbors: [String]) -> () {
+    print ("I sit next to \(neighbors[0]) and \(neighbors[1])")
+}
+mySeatingArrangement(neighbors: ["Anjali", "Lydia"])
+// --------
+var first30Fibonacci = 0..<30
+first30Fibonacci.map()
+var first = 0
+var second = 1
+for _ in 0..number {
+    let prev = first
+    first = second
+    second = prev + first
+}
+return first
+}
 
 //1)
 
