ea738c132e7feecb4f38361cfe865285fbdd937a
Abdulaziz Albahar <abdulaziz@albahar.net>
2024-09-23 18:00:41 -0400

    challenge w3d1

---
+++ Week_Three/Session_One/TODO4/TODO4/TODO4App.swift
@@ -0,0 +1,17 @@
+//
+//  TODO4App.swift
+//  TODO4
+//
+//  Created by Abdulaziz Albahar on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct TODO4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
