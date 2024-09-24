7f7e33d6483c39bcb71479a3bb96cac3eff6ad98
rbokil <rini.bokil@gmail.com>
2024-09-24 19:26:56 -0400

    todo 4

--- Week_Three/Session_One/TODO4/TODO4/ContentView.swift
+++ Week_Three/Session_One/TODO4/TODO4/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, Swift!")
+                .font(.largeTitle)
+                .foregroundColor(.blue)
+                .padding()
+                .frame(width: 300, height: 100)
+                .background(.yellow)
         }
         .padding()
     }
