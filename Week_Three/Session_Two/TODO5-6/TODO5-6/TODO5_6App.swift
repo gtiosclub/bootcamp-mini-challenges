fafe0d07d1e3d469e07beb8020d7d5a5649be2af
Abdulaziz Albahar <abdulaziz@albahar.net>
2024-09-25 22:53:19 -0400

    ui challenge

---
+++ Week_Three/Session_Two/TODO5-6/TODO5-6/TODO5_6App.swift
@@ -0,0 +1,17 @@
+//
+//  TODO5_6App.swift
+//  TODO5-6
+//
+//  Created by Abdulaziz Albahar on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TODO5_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            TODO5()
+        }
+    }
+}
