e3d604bf496e33b35f84c6a4f46acde6857a0752
Abdulaziz Albahar <abdulaziz@albahar.net>
2024-09-30 16:27:38 -0400

    wk4

---
+++ Week_Four/Session_One/Task1&2/Task1&2/Task2/CustomText.swift
@@ -0,0 +1,18 @@
+//
+//  CustomText.swift
+//  Task1&2
+//
+//  Created by Abdulaziz Albahar on 9/30/24.
+//
+
+import SwiftUI
+
+struct CustomText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomText()
+}
