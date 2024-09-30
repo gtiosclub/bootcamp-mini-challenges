b41ca6924af3d860446ee47b54c3726f7ac1fa24
Abdulaziz Albahar <abdulaziz@albahar.net>
2024-09-30 16:27:38 -0400

    wk4

---
+++ Week_Four/Session_One/Task1&2/Task1&2/Task2/HomePage.swift
@@ -0,0 +1,18 @@
+//
+//  HomePage.swift
+//  Task1&2
+//
+//  Created by Abdulaziz Albahar on 9/30/24.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePage()
+}
