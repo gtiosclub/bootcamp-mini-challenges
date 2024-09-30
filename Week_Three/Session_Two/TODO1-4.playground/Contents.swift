bb393cb032004ec9e8b22f7407bc42747175fcdd
Eternal-Lost <a972461825@icloud.com>
2024-09-30 18:26:36 -0400

    Stuff

--- Week_Three/Session_Two/TODO1-4.playground/Contents.swift
+++ Week_Three/Session_Two/TODO1-4.playground/Contents.swift
@@ -2,9 +2,59 @@
 //----------------------------------------------------------------------------//Part 1 - Swift\\----------------------------------------------------------------------\\
 
 //1)
+struct Rectangle{
+    var width: Double
+    var height: Double
+    
+    //Implicit initializer is provided already
+    
+    //This is NOT a closure and NOT a stored property - It's a computed property
+    var area : Double{
+        return width*height
+    }
+}
+let rectangle1 = Rectangle(width: 10, height: 5)
+print (rectangle1)
+
 
 //2)
+struct Point{
+    var x: Int
+    var y: Int
+}
+let point1 = Point(x:10, y:20)
+var point2 = point1
+
+//point1.x = 4
+print(point1)
+
 
 //3)
+class Circle {
+    var radius: Double
+    
+    init(radius: Double){
+        self.radius = radius
+    }
+    func Circumference() -> Double{
+        return 2*3.14*radius
+    }
+}
+
+let circle1 = Circle(radius: 2.0)
+print(circle1)
 
 //4)
+/*
+class Car{
+    var model : String
+ 
+    init(model: String){
+        self.model = model1
+    }
+}
+*/
+
+var body: some View {
+    
+}
--- Week_Two/Session_One/TODO.playground/Contents.swift
+++ Week_Two/Session_One/TODO.playground/Contents.swift
@@ -9,8 +9,52 @@ Additionally, please feel free to ask your instructor if you need any help!
 //----------------------------------------------------------------------------//Part 2 - Coding\\----------------------------------------------------------------------\\
 
 //1)
+var arr = [1, 2, 3, 4, 5]
+arr.append(6)
+arr.remove(at: 1)
 
 //2)
+var age : [Int] = [90, 85, 88]
+
+var names : [String] = ["John", "Jane", "Jake"]
 
 //3)
 
+var uniqueNumbers: Set<Int> = [1, 2, 3, 4, 5]
+//uniqueNumbers.append(5)
+
+//4)
+var fruits = ["Apple", "Banana", "Cherry"]
+for n in 0..<3{
+    print(fruits[n])
+}
+
+//5)
+var num = 1
+while num <= 5 {
+    print(num)
+    num += 1
+}
+
+//6)
+var ages : [String:Int] = ["Alice" : 30, "Bob" : 25, "Charlie" : 35]
+
+for z in 0..<3{
+    print(ages[z])
+}
+
+//7)
+func addNumbers(_ num1: Int, _ num2: Int) -> Int{
+    return num1+num2
+}
+
+print(addNumbers(3, 7))
+/*
+//Mini Challenge
+
+var months: [Int : String] = [:]
+months = [
+    1: "January",
+    2: "Febbuary"
+]
+*/
