621e4d90c8d8e8e30b964507c13732c3f1882a56
esanint <esanint05@gmail.com>
2024-10-02 19:17:30 -0400

    Completed W4D2 Task 2

---
+++ Week_Four/Session_Two/Tasks/Tasks/AnimalModel.swift
@@ -0,0 +1,9 @@
+//
+//  AnimalModel.swift
+//  Tasks
+//
+//  Created by Esteban Sanint on 10/2/24.
+//
+
+import Foundation
+
--- Week_Four/Session_Two/Tasks/Tasks/ContentView.swift
+++ Week_Four/Session_Two/Tasks/Tasks/ContentView.swift
@@ -6,19 +6,55 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
+    let animals: [Animal] = [
+            Animal(id: UUID(), name: "Lion", type: .mammal),
+            Animal(id: UUID(), name: "Eagle", type: .bird),
+            Animal(id: UUID(), name: "Crocodile", type: .reptile),
+            Animal(id: UUID(), name: "Elephant", type: .mammal),
+            Animal(id: UUID(), name: "Parrot", type: .bird)
+    ]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(animals) { animal in
+                VStack(alignment: .leading) {
+                    Text(animal.name)
+                        .font(.headline)
+                    Text(animal.type.title)
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+            }
+            .navigationTitle("Animals")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+enum AnimalCategory {
+    case mammal, reptile, bird
+
+    var title: String {
+        switch self {
+        case .mammal:
+            return "Mammal"
+        case .reptile:
+            return "Reptile"
+        case .bird:
+            return "Bird"
+        }
+    }
+}
+
+struct Animal: Identifiable, Equatable {
+    let id: UUID
+    let name: String
+    let type: AnimalCategory
 }
