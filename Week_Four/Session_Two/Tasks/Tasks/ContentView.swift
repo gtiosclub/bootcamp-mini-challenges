6f53d4d85028f9dbc8aae48ab0a0b18cb05de90c
Matthew Dong <mattdong1231@gmail.com>
2024-10-03 23:52:03 -0400

    week 4 session 2 minichallenges

--- Week_Four/Session_Two/Tasks/Tasks/ContentView.swift
+++ Week_Four/Session_Two/Tasks/Tasks/ContentView.swift
@@ -8,16 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    let animals = [Animal(id: UUID(), name: "Lion", type: .mammal), Animal(id: UUID(), name: "Lizard", type: .reptile)]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List(animals) {animal in VStack(alignment: .leading) {
+                Text("Name: \(animal.name)").font(.headline)
+                Text("Type: \(animal.type.title)").font(.subheadline)
+            }
+                
+            }
+            
+            
+            
         }
         .padding()
     }
 }
+
+enum AnimalCategory {
+    case mammal
+    case reptile
+    case bird
+    
+    var title: String {
+        switch self {
+        case .mammal: return "mammal"
+        case .reptile: return "reptile"
+        case.bird: return "bird"
+        }
+    }
+}
+
+struct Animal: Identifiable, Equatable {
+    var id: UUID
+    var name: String
+    var type: AnimalCategory
+}
+func main() {
+    let animal1 = Animal(id: UUID(), name: "Lion", type: .mammal)
+    let animal2 = Animal(id: UUID(), name: "Lion", type: .mammal)
+
+    let isEqual = animal1 == animal2
+    print("\(isEqual)")
+    
+}
+
+
+
 
 #Preview {
     ContentView()
